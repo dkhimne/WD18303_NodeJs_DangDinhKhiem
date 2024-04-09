07e3e80de87abcbb984904d47e83d072a7175a46
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-04-09 22:31:58 +0700

    ASM

--- Assignment/routes/admin.js
+++ Assignment/routes/admin.js
@@ -1,5 +1,6 @@
 const ProductController = require ("../controllers/admin/ProductController");
 const CategoryController = require ("../controllers/admin/CategoryController")
+const UserController = require ("../controllers/admin/UserController")
 
 const multer = require('multer');
 const express  = require("express");
@@ -16,11 +17,15 @@ const storage = multer.diskStorage({
 const uploads = multer({ storage: storage })
 
 //product
-router.get('/listproducts',ProductController.getProducts); // /listproducts tự đặt
+router.get('/listproducts',ProductController.getProducts); 
 
 router.get('/themproduct',ProductController.AddProduct);
 
 router.post('/themproduct',uploads.single('image'),ProductController.postAddProduct);
+
+router.get('/delete/:id', ProductController.delProduct);
+
+router.delete('/delete/:id', ProductController.delProduct);
 
 //category
 router.get('/listcategories',CategoryController.getCategories);
@@ -33,5 +38,15 @@ router.get('/delete/:id', CategoryController.delCategory);
 
 router.delete('/delete/:id', CategoryController.delCategory);
 
+//user
+router.get('/listkh',UserController.getUsers);
+
+router.get('/themkh',UserController.AddUser);
+
+router.post('/themkh',UserController.postAddUser);
+
+router.get('/delete/:id', UserController.delUser);
+
+router.delete('/delete/:id', UserController.delUser);
 
 module.exports = router;
